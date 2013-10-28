b024784f0d91697d1d6a9660e864016f4b92f15a
henrygauer <henrygauer@gmx.de>
2013-10-28 10:48:18 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//DE" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="de">
+<head profile="http://gmpg.org/xfn/11">
+ 
+   <!-- wird noch ausgefüllt -->
+ 
+</head>
+<body>
+ 
+<div id="wrapper">
+ 
+   <div id="header"></div><!-- header -->
+ 
+   <div id="main"></div><!-- main -->
+ 
+   <div id="sidebar"></div><!-- sidebar --> 
+ 
+   <div id="footer"></div><!-- footer -->
+ 
+</div><!-- wrapper -->
+ 
+</body>
+</html>
