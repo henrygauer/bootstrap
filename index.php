bd8ad8fbdecb6b246b6864ae64d43fbd3bee97df
Oliver Thierfelder <henrygauergmx.de>
2013-10-30 01:15:35 +0100

    Commit Aufgabe2

--- index.php
+++ index.php
@@ -2,10 +2,40 @@
     <html>
     <head>
     <title>Bootstrap 101 Template</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-    <h1>Hello, world!</h1><br>
+    <style type="text/css">
+	<!-- Font { font-family: Arial,FranklinGothic,Geneva,Helvetica; } -->
+	</style>
+	<style>
+	body
+	{
+	color:#cccccc;
+	}
+	</style>
+	<style>
+	a:visited
+	{
+	color:#cc9900
+	}
+	</style>
+	<style>
+	a:link
+	{
+	color:#ff057f
+	}
+	</style>
+	<style>
+	a:active
+	{
+	color:#cc9900
+	}
+	</style>
+	<!-- Die dazu fehlenden Links und Bilder folgen noch! -->
+	</head>
+    <body style="background-color:#000000"> 
+    <h1>Hello, world!</h1>
+    <br>
     Oliver Thierfelder   
     <script src="http://code.jquery.com/jquery.js"></script>
     </body>
